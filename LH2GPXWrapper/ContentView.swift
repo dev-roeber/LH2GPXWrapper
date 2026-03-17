56f81ab70606d52bf3301166b3e831fb6c6dafe5
de.roeber <206363291+dev-roeber@users.noreply.github.com>
2026-03-17 05:00:58 +0100

    Initial Commit

---
+++ LH2GPXWrapper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LH2GPXWrapper
+//
+//  Created by Sebastian on 17.03.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
